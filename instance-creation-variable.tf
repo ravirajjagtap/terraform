d423644fb1a87d0e99725d673ad8186e0eb04ee9
ravirajjagtap <156221418+ravirajjagtap@users.noreply.github.com>
2024-02-13 07:28:57 +0000

    aa

--- instance-creation-variable.tf
+++ instance-creation-variable.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "ins-01-var"{
         Name = "my-inst-var-1"
     }
     ami=var.image_id
-    instance_type="t2.micro"
+    instance_type=var.ins_type
     key_name="tokyo-key"
     vpc_security_group_ids=["sg-09b565401492535fc"]
 }
@@ -15,7 +15,7 @@ resource "aws_instance" "ins-02-var"{
         Name = "my-inst-var-2"
     }
     ami=var.image_id
-    instance_type="t2.micro"
+    instance_type=var.ins_type
     key_name="tokyo-key"
     vpc_security_group_ids=["sg-09b565401492535fc"]
 }
@@ -24,7 +24,7 @@ resource "aws_instance" "ins-03-var"{
         Name = "my-inst-var-3"
     }
     ami=var.image_id
-    instance_type="t2.micro"
+    instance_type=var.ins_type
     key_name="tokyo-key"
     vpc_security_group_ids=["sg-09b565401492535fc"]
 }
@@ -33,10 +33,13 @@ resource "aws_instance" "ins-04-var"{
         Name = "my-inst-var-4"
     }
     ami=var.image_id
-    instance_type="t2.micro"
+    instance_type=var.ins_type
     key_name="tokyo-key"
     vpc_security_group_ids=["sg-09b565401492535fc"]
 }
 variable "image_id"{
     default="ami-07c589821f2b353aa"
+}
+variable "ins_type" {
+    default = "t3.micro"
 }
