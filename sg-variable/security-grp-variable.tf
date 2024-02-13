4837b3d785c247b3fde4f540ab57739ebafb3c07
ravirajjagtap <156221418+ravirajjagtap@users.noreply.github.com>
2024-02-13 17:14:22 +0000

    new

--- sg-variable/security-grp-variable.tf
+++ sg-variable/security-grp-variable.tf
@@ -1,9 +1,7 @@
-provider "aws" 
-{
+provider "aws" {
     region="ap-northeast-1"
 }
-resource "aws_instance" "my_instance-1"
-{
+resource "aws_instance" "my_instance-1"{
     tags={
         Name ="rule-var-inst-1"    }
     ami="ami-07c589821f2b353aa"
@@ -11,10 +9,10 @@ resource "aws_instance" "my_instance-1"
     key_name="tokyo-key"
     vpc_security_group_ids=["sg-09b565401492535fc"]
 }
-resource "aws_instance" "my_instance-2"
-{
+resource "aws_instance" "my_instance-2"{
     tags={
-        Name ="rule-var-inst-2"    }
+        Name ="rule-var-inst-2"    
+    }
     ami="ami-07c589821f2b353aa"
     instance_type="t2.micro"
     key_name="tokyo-key"
