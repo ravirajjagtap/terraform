ea7712949bd89d9b051c08c4615865eaad056af6
ravirajjagtap <156221418+ravirajjagtap@users.noreply.github.com>
2024-02-13 06:15:35 +0000

    AA

--- simple-instance-creation.tf
+++ simple-instance-creation.tf
@@ -5,17 +5,8 @@ resource "aws_instance" "my_instance" {                       #"resource_type" "
     tags ={
         Name="inst01"
         }
-    ami="ami-0b5c74e235ed808b9"
+    ami="ami-0204c9bcdc2f0250d"
     instance_type="t2.micro"
-    key_name="tokyo-key"
-    vpc_security_group_ids=["sg-0c9d82f928d058f7d"]   
-}
-resource "aws_instance" "my_instance-2" {
-    tags ={
-        Name="inst02"
-        }
-    ami="ami-0b5c74e235ed808b9"
-    instance_type="t2.large"
     key_name="tokyo-key"
     vpc_security_group_ids=["sg-0c9d82f928d058f7d"]   
 }
