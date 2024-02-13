2481d6d7325e122b1b3063151416132dd4b7bd8f
ravirajjagtap <156221418+ravirajjagtap@users.noreply.github.com>
2024-02-13 07:05:04 +0000

    AAA

---
+++ instance-creation-variable.tf
@@ -0,0 +1,12 @@
+provider "aws"{
+    region="ap-northeast-1"
+}
+resource "aws_instance" "ins-01-var"{
+    tags={
+        Name = "my-inst-var-1"
+    }
+    ami="ami-0c7217cdde317cfec"
+    instance_type="t2.micro"
+    key_name="tokyo-key"
+    vpc_security_group_ids=["sg-09b565401492535fc"]
+}
