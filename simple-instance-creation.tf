15ba0eb80b46a686664eb2084159325208fe4f2d
ravirajjagtap <156221418+ravirajjagtap@users.noreply.github.com>
2024-02-13 05:46:40 +0000

    aaa

--- simple-instance-creation.tf
+++ simple-instance-creation.tf
@@ -2,7 +2,9 @@ provider "aws"{
     region="ap-northeast-1"
 }
 resource "aws_instance" "my_instance" {
-    tags{Name="inst01"}
+    tags{
+        Name="inst01"
+        }
     ami="ami-0b5c74e235ed808b9"
     instance_type="t2.micro"
     key_name="tokyo-key"
