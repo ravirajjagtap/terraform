224cbd9ee6b2fc18d680b4ecf61512abd88a470a
ravirajjagtap <156221418+ravirajjagtap@users.noreply.github.com>
2024-02-13 17:32:31 +0000

    aa

--- sg-variable/security-grp-variable.tf
+++ sg-variable/security-grp-variable.tf
@@ -5,7 +5,7 @@ resource "aws_instance" "my_instance-1"{
     tags={
         Name ="rule-var-inst-1"   
     }
-    ami="ami-07c589821f2b353aa"
+    ami=var.ami
     instance_type="t2.micro"
     key_name="tokyo-key"
     vpc_security_group_ids=["sg-09b565401492535fc"]
@@ -14,8 +14,31 @@ resource "aws_instance" "my_instance-2"{
     tags={
         Name ="rule-var-inst-2"    
     }
-    ami="ami-07c589821f2b353aa"
+    ami=var.ami
     instance_type="t2.micro"
     key_name="tokyo-key"
     vpc_security_group_ids=["sg-09b565401492535fc"]
+}
+
+resource "aws_security_group" "my_sg"{
+    Name="sg_grp"
+    description="allow-ssh and http"
+    vpc_id="vpc-0d4f640bc61acb3bb"
+    ingress{
+        protocol="TCP"
+        from_port=22
+        to_port=22
+        cidr_block=["0.0.0.0/0"]
+    }
+    egress{
+        protocol="-1"
+        from_port=0
+        to_port=0
+        cidr_block=["0.0.0.0/0"]
+    }
+}
+
+
+variable "ami"{
+    default ="ami-07c589821f2b353aa"
 }
